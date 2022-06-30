4bb134410e870c7467c372f769e01accd82f2fc6
Torikul Islam <toriqulislam142@gmail.com>
2022-06-30 13:32:08 -0500

    created api for update data

--- index.js
+++ index.js
@@ -2,49 +2,79 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
-
-app.get('/', (req,res)=>{
-    res.send('todo server is running')
-})
-
 // connect here mongobd
-
-
 
 const uri = `mongodb+srv://todo-spot:${process.env.PASS}@cluster0.tdgos.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// client.connect(err => {
-  
-//   console.log('mongo is connected')
-//   client.close();
-// });
 
-async function main(){
-    try{
+async function main() {
+    try {
         await client.connect()
-
         const todosCollection = client.db('todo-spot').collection('todos')
 
-        app.get('/todos', async(req,res)=>{
+        app.get('/todos', async (req, res) => {
             const query = {}
+            const todos = await todosCollection.find(query).toArray()
+            res.send(todos)
         })
-    
+        app.post('/addTodos', async (req, res) => {
+            const todo = req.body
+            const result = await todosCollection.insertOne(todo)
+            res.send(result)
+        })
+
+        app.put('/completeTodo/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+
+            const updateDoc = {
+                $set: {
+                    isComplete: true
+                }
+            }
+            const result = await todosCollection.updateOne(query, updateDoc);
+            res.send(result);
+        })
+
+        // update todos
+        app.put('/updateTodo/:id', async(req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const data = req.body
+            const title = data.title
+            const description = data.description
+            const option = { upser: true }
+
+            const updateDoc = {
+                $set: {
+                    title: title,
+                    description: description
+                }
+            }
+            const result = await todosCollection.updateOne(query, updateDoc, option);
+            res.send(result);
+        })
+
+
+
     }
-    finally{
+    finally {
 
     }
 }
 main()
 
+app.get('/', (req, res) => {
+    res.send('todo server is running')
+})
 
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('Todo server is runnin on', port)
 })
